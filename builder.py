f2f36cd17f8ff6a75a66e11a3b6f3bcff903e0b8
duccdev <duccybaka@gmail.com>
2024-10-07 16:28:32 +0300

    refactor: just use stderr argument

--- builder.py
+++ builder.py
@@ -1,6 +1,7 @@
 import utils
 import subprocess
 import os
+import sys
 from typing import Literal
 
 
@@ -33,27 +34,31 @@ def build(
             f.write(source)
 
         nasm_output = subprocess.run(
-            ["nasm", f"-felf{bits}", f"/tmp/{id}.s", "-o", f"/tmp/{id}.o"]
+            ["nasm", f"-felf{bits}", f"/tmp/{id}.s", "-o", f"/tmp/{id}.o"],
+            stderr=sys.stderr,
         )
 
         if nasm_output.returncode != 0:
-            raise AssemblerError(nasm_output.stderr.decode("utf-8", errors="ignore"))
+            raise AssemblerError()
 
         ld_args = ["ld", f"/tmp/{id}.o", "-o", output_path]
         if bits == 32:
             ld_args.append("-m")
             ld_args.append("elf_i386")
 
-        ld_output = subprocess.run(ld_args)
+        ld_output = subprocess.run(ld_args, stderr=sys.stderr)
 
         if ld_output.returncode != 0:
-            raise LinkerError(ld_output.stderr.decode("utf-8", errors="ignore"))
+            raise LinkerError()
 
         if strip:
-            strip_output = subprocess.run(["strip", output_path])
+            strip_output = subprocess.run(
+                ["strip", output_path],
+                stderr=sys.stderr,
+            )
 
             if strip_output.returncode != 0:
-                raise StripError(strip_output.stderr.decode("utf-8", errors="ignore"))
+                raise StripError()
     except Exception as e:
         try:
             os.remove(output_path)
