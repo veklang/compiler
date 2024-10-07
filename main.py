f6985d38b124f01b5afa6b492fc448caa19992d7
duccdev <duccybaka@gmail.com>
2024-10-07 16:30:15 +0300

    feat: hi mom binary

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from emitter import Emitter
 from builder import build
 
 emitter = Emitter()
-emitter.syscall_exit(69)
+emitter.syscall_write(1, "hi mom\n".encode("ascii"))
+emitter.syscall_exit(0)
 
-build(emitter.asm, strip=True, output_path="exit69")
+build(emitter.asm, strip=True, output_path="himom")
