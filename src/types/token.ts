0be9d279eae2ba0088d76ead52880a8bbf151f61
duccdev <duccybaka@gmail.com>
2024-12-04 07:09:40 +0300

    refactor: separate stuff

--- src/types/token.ts
+++ src/types/token.ts
@@ -1,87 +1,104 @@
+export const keywords = [
+  "in",
+  "return",
+  "exit",
+  "if",
+  "else",
+  "match",
+  "for",
+  "while",
+  "break",
+  "continue",
+  "let",
+  "const",
+  "as",
+  "int",
+  "i8",
+  "i16",
+  "i32",
+  "i64",
+  "uint",
+  "u8",
+  "u16",
+  "u32",
+  "u64",
+  "float",
+  "f16",
+  "f32",
+  "f64",
+  "char",
+  "string",
+  "bool",
+  "void",
+  "null",
+  "enum",
+  "struct",
+  "alias",
+  "Array",
+  "Tuple",
+  "Map",
+  "Callable",
+  "class",
+  "constructor",
+  "destructor",
+  "extends",
+  "implements",
+  "abstract",
+  "pub",
+  "getter",
+  "setter",
+  "static",
+  "drop",
+  "alloc",
+  "typeof",
+  "sizeof",
+  "inline",
+  "fn",
+  "mod",
+  "use",
+] as const;
+
+export const literals = [
+  "Identifier",
+  "String",
+  "Integer",
+  "Float",
+  "True",
+  "False",
+  "Null",
+] as const;
+
+export const punctuation = [
+  "LeftParen",
+  "RightParen",
+  "LeftBrace",
+  "RightBrace",
+  "Comma",
+  "Dot",
+  "Semicolon",
+] as const;
+
+export const operators = [
+  "Minus",
+  "Plus",
+  "Slash",
+  "Asterisk",
+  "Bang",
+  "BangEqual",
+  "Equal",
+  "EqualEqual",
+  "Greater",
+  "GreaterEqual",
+  "Less",
+  "LessEqual",
+] as const;
+
 export type TokenType =
-  | "Punctuation:LeftParen"
-  | "Punctuation:RightParen"
-  | "Punctuation:LeftBrace"
-  | "Punctuation:RightBrace"
-  | "Punctuation:Comma"
-  | "Punctuation:Dot"
-  | "Punctuation:Semicolon"
-  | "Operator:Minus"
-  | "Operator:Plus"
-  | "Operator:Slash"
-  | "Operator:Asterisk"
-  | "Operator:Bang"
-  | "Operator:BangEqual"
-  | "Operator:Equal"
-  | "Operator:EqualEqual"
-  | "Operator:Greater"
-  | "Operator:GreaterEqual"
-  | "Operator:Less"
-  | "Operator:LessEqual"
-  | "Keyword:in"
-  | "Keyword:return"
-  | "Keyword:exit"
-  | "Keyword:if"
-  | "Keyword:else"
-  | "Keyword:match"
-  | "Keyword:for"
-  | "Keyword:while"
-  | "Keyword:break"
-  | "Keyword:continue"
-  | "Keyword:let"
-  | "Keyword:const"
-  | "Keyword:as"
-  | "Keyword:int"
-  | "Keyword:i8"
-  | "Keyword:i16"
-  | "Keyword:i32"
-  | "Keyword:i64"
-  | "Keyword:uint"
-  | "Keyword:u8"
-  | "Keyword:u16"
-  | "Keyword:u32"
-  | "Keyword:u64"
-  | "Keyword:float"
-  | "Keyword:f16"
-  | "Keyword:f32"
-  | "Keyword:f64"
-  | "Keyword:char"
-  | "Keyword:string"
-  | "Keyword:bool"
-  | "Keyword:void"
-  | "Keyword:null"
-  | "Keyword:enum"
-  | "Keyword:struct"
-  | "Keyword:alias"
-  | "Keyword:Array"
-  | "Keyword:Tuple"
-  | "Keyword:Map"
-  | "Keyword:Callable"
-  | "Keyword:class"
-  | "Keyword:constructor"
-  | "Keyword:destructor"
-  | "Keyword:extends"
-  | "Keyword:implements"
-  | "Keyword:abstract"
-  | "Keyword:pub"
-  | "Keyword:getter"
-  | "Keyword:setter"
-  | "Keyword:static"
-  | "Keyword:drop"
-  | "Keyword:alloc"
-  | "Keyword:typeof"
-  | "Keyword:sizeof"
-  | "Keyword:inline"
-  | "Keyword:fn"
-  | "Keyword:mod"
-  | "Keyword:use"
-  | "Literal:Identifier"
-  | "Literal:String"
-  | "Literal:Integer"
-  | "Literal:Float"
-  | "Literal:True"
-  | "Literal:False"
-  | "Literal:Null";
+  | "Punctuation"
+  | "Operator"
+  | "Keyword"
+  | "Literal"
+  | "Identifier";
 
 export interface Token {
   type: TokenType;
